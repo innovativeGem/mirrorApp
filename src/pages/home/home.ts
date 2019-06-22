ec950960123c1a38ca1ffa320dd22d2bdc9c2820
@innovativeGem <naman.nanavati@gmail.com>
2019-06-22 17:51:17 +1000

    added brightness plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from 'ionic-angular';
+
+import { Brightness } from '@ionic-native/brightness/ngx';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
+  screenWidth: number;
+  screenHeight: number;
+  brightnessVal: number;
 
+  constructor(
+    private platform: Platform,
+    private brightness: Brightness
+    ) {}
+
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      this.screenWidth = window.outerWidth;
+      this.screenHeight = window.outerHeight;
+    });
+    /*
+    this.platform.ready().then(() => {
+      console.log(this.platform.width());
+      this.getWidth();
+    });
+    */
   }
 
+  getFrameColor() {
+    const el = <HTMLElement>document.querySelector('.first-frame');
+    const newColor = "#" + Math.floor(100000 + Math.random() * 900000).toString();
+    el.style.borderColor = newColor;
+  }
+
+  handleBrightnessChange(event) {
+    this.brightnessVal = event.details.value;
+    console.log(this.brightnessVal);
+    this.brightness.setBrightness(this.brightnessVal);
+  }
 }
